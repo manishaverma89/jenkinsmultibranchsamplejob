e7726a13d748bdccb5318e6b31aaec9ce30e388b
manishaverma89 <84954924+manishaverma89@users.noreply.github.com>
2023-01-04 06:15:06 +0530

    Update whatisjenkinsfile.py

--- whatisjenkinsfile.py
+++ whatisjenkinsfile.py
@@ -1,1 +1,2 @@
 print ("Jenkinsfile is a text file that contains the definition of a Jenkins Pipeline and is checked into source control.")
+print("Next we will learm about different type of jenkins jobs")
